26e630dc634258425b7526988cfd9940ac560937
Bowen <tbow1382@740s-pl-120.local>
2019-01-22 09:18:15 -0700

    Initial commit

---
+++ FirstProject/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  FirstProject
+//
+//  Created by Bowen, Tanner on 1/22/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Am I predictable?\n";
+    return 0;
+}
